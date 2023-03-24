9990ae5d3312c5d8067ebe6e05289b0fef55263b
Daniel Tuiran <danny0228@hotmail.com>
2023-03-24 19:17:39 -0400

    firstclass functions

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    return callback()
+}
+
+function returnsANamedFunction() {
+    return function aNamedFunction() {
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return (function () {
+
+    })
+}
+
+
+
